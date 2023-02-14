0b8b0ece86668171df9e9f5de63a1e9cebef9537
billycraig2 <91530897+billycraig2@users.noreply.github.com>
2023-02-14 23:58:23 +0000

    Create roman.cpp

---
+++ roman.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "functions.hpp"
+
+int main() {
+    std::cout << romanToInt("III") << "\n";
+    std::cout << romanToInt("LVIII") << "\n";
+    std::cout << romanToInt("MCMXCIV") << "\n";
+}
